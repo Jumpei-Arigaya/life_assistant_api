b47a0f5c307d271500c962934a56b05abd0cb33b
Jumpei-Arigaya <junpeeei0407@gmail.com>
2024-04-13 13:59:05 +0900

    add date

--- api/family/models.py
+++ api/family/models.py
@@ -6,3 +6,5 @@ class People(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
